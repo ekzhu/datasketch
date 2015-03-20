614119a9965f33b160953f73349ea17331c4bc74
Eric Zhu <erkangzhu@gmail.com>
2015-03-19 21:48:39 -0400

    add summary

---
+++ sksketch/__init__.py
@@ -0,0 +1,8 @@
+"""
+Data sketch module for Python
+===============================
+
+sksketch is a Python module integrating various data sketch
+algorithms. It aims to provide efficient approximation alternatives
+to exact solutions to data mining and data integration problems.
+"""
