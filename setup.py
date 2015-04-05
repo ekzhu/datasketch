73a82f7a35150de8efc085f77bf06cd8bf9c3e72
Eric Zhu <erkangzhu@gmail.com>
2015-04-05 00:34:02 -0400

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.4',
+    version='0.1.6',
 
     description='Probabilistic data structures for processing very large datasets',
     long_description=long_description,
