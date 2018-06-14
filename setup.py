b8d674725691cacf326e2a5032c62e4f61ab08b7
aastafiev <aastafiev@gmail.com>
2018-06-14 10:30:03 +0300

    added AsyncMinHashLSH to lib

--- setup.py
+++ setup.py
@@ -74,7 +74,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.11', 'redis>=2.10.0', 'aioredis>=1.1.0'],
+    # install_requires=['numpy>=1.11', 'redis>=2.10.0', 'aioredis>=1.1.0'],
+    install_requires=['numpy>=1.11', 'redis>=2.10.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
