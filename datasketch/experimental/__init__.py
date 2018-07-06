ecf8999c0708731e3b56f20166d664c9b902f319
Eric Zhu <ekzhu@cs.toronto.edu>
2018-07-06 15:34:12 -0400

    add experimental module

---
+++ datasketch/experimental/__init__.py
@@ -0,0 +1,11 @@
+"""
+WARNING:
+
+datasketch.experimental is dedicated to new modules that are to be merged into
+the stable interface of datasketch. So their interfaces may change in future
+versions.
+
+To add a new class or function, register it here in this file. For example:
+
+from new_module import NewModuleClass
+"""
