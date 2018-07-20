2d605cd6fe8869e6934198269ed50438405aa43d
aastafiev <aastafiev@gmail.com>
2018-07-20 17:32:13 +0300

    python version checks

--- datasketch/experimental/async/__init__.py
+++ datasketch/experimental/async/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info < (3, 6):
+if sys.version_info >= (3, 6):
     from datasketch.experimental.async.lsh import AsyncMinHashLSH
     # Alias
     AsyncWeightedMinHashLSH = AsyncMinHashLSH
