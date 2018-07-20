f91d7449091bf571ce5313d04446db32a8b8b828
aastafiev <aastafiev@gmail.com>
2018-07-20 16:13:22 +0300

    updated requirement packages

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.11', 'redis>=2.10.0'],
+    install_requires=['numpy>=1.11', 'redis>=2.10.0', 'aioredis', 'motor', 'pymongo'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
