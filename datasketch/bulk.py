b4b2964e0ae3b75ab447de892d80711dad27e1f1
Jordan Martin <jordan@heimdall.llc>
2020-11-26 14:08:30 -0800

    replaced type check with iterable instead of list

--- datasketch/bulk.py
+++ datasketch/bulk.py
@@ -1,4 +1,5 @@
 from datasketch.minhash import MinHash
+from collections.abc import Iterable
 
 def compute_minhash(b, m):
     return MinHash(
@@ -27,14 +28,14 @@ def compute_minhashes(b, m):
 def compute_minhashes_generator(b, m):
     '''Helper method to compute minhashes in bulk. This helper avoids unnecessary
     overhead when initializing many minhashes by reusing initial state. This method
-    returns a generator for steaming computation.
+    returns a generator for streaming computation.
 
     Args:
-        b (List[List[bytes]]): List containing lists of bytes
+        b (iterable): Iterable containing lists of bytes
         m (MinHash): Initialized MinHash object
             The configuration of this MinHash will be used for all minhashes
     '''
     m = MinHash() if m is None else m
-    assert isinstance(b, list), TypeError(f'Expecting List[List[bytes]], given: {type(b)}')
+    assert isinstance(b, Iterable), TypeError(f'Expecting iterable, given: {type(b)}')
     for _b in b:
         yield compute_minhash(_b, m)
