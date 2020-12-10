cf6d447041396f12e4e0e6474b35ed5f2f9f35f7
Jordan Martin <jordan@heimdall.llc>
2020-12-10 10:45:52 -0800

    updated tests

--- test/test_bulk.py
+++ test/test_bulk.py
@@ -5,19 +5,13 @@ from test.utils import fake_hash_func
 
 
 class TestBulk(unittest.TestCase):
-
-    def test_compute_minhash(self):
-        b = [n*4 for n in range(4)]
-        m1 = minhash.MinHash(4, 1, hashfunc=fake_hash_func)
-        m2 = bulk.compute_minhash(b, m1)
-        m1.update_batch(b)
-        self.assertTrue(np.array_equal(m1.hashvalues, m2.hashvalues))
     
     def test_compute_minhashes(self):
+        kwargs = dict(num_perm=4, seed=1, hashfunc=fake_hash_func)
         b = [[n*4 for n in range(4)]]*2
-        m1 = minhash.MinHash(4, 1, hashfunc=fake_hash_func)
-        m2, m3 = bulk.compute_minhashes(b, m1)
+        m1 = minhash.MinHash(**kwargs)
         m1.update_batch(b[0])
+        m2, m3 = bulk.compute_minhashes(b, **kwargs)
         self.assertTrue(np.array_equal(m1.hashvalues, m2.hashvalues))
         self.assertTrue(np.array_equal(m1.hashvalues, m3.hashvalues))
 
