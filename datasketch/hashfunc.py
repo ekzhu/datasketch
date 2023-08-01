a136831acf3528ce903e78cc7a9cc90f0f30c334
Chris Ha <hac541309@gmail.com>
2023-08-02 00:49:53 +0900

    Update hashfunc.py (#211)

--- datasketch/hashfunc.py
+++ datasketch/hashfunc.py
@@ -13,7 +13,7 @@ def sha1_hash32(data):
     return struct.unpack('<I', hashlib.sha1(data).digest()[:4])[0]
 
 def sha1_hash64(data):
-    """A 32-bit hash function based on SHA1.
+    """A 64-bit hash function based on SHA1.
 
     Args:
         data (bytes): the data to generate 64-bit integer hash from.
