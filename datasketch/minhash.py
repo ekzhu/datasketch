85d78d93c9f1f8077784ac92771fe82630b764f0
chris-ha458 <hac541309@gmail.com>
2023-08-27 22:11:29 +0900

    tile is now unnecessary

--- datasketch/minhash.py
+++ datasketch/minhash.py
@@ -157,7 +157,7 @@ class MinHash(object):
         '''
         hv = np.array([self.hashfunc(_b) for _b in b], dtype=np.uint64,ndmin=2).T
         a, b = self.permutations
-        phv = np.bitwise_and(((hv * np.tile(a, (len(hv), 1))) + b) % _mersenne_prime, _max_hash)
+        phv = (hv * a + b) % _mersenne_prime & _max_hash
         self.hashvalues = np.vstack([phv, self.hashvalues]).min(axis=0)
 
     def jaccard(self, other):
