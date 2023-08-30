0d16872d1eee711fe75246d5cfaedd443d1cf8b0
ekzhu <ekzhu@users.noreply.github.com>
2023-08-30 19:40:58 +0000

    Deploying to gh-pages from @ ekzhu/datasketch@da9dd79722d6d0054bfa797bd1eeab3c59c51dab 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.9',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.6.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
