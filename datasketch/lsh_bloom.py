5771be3546b524d0309ccd19def7ffa31cbe1dde
Arham Khan <arhammkhan@gmail.com>
2025-05-27 18:45:23 -0500

    clean up lshbloom impl

--- datasketch/lsh_bloom.py
+++ datasketch/lsh_bloom.py
@@ -5,6 +5,7 @@ from datasketch.b_bit_minhash import bBitMinHash
 from scipy.integrate import quad as integrate
 import numpy as np
 from pybloomfilter import BloomFilter
+import warnings
 import os
 
 _mersenne_prime = np.uint64((1 << 61) - 1)
@@ -39,84 +40,85 @@ def _optimal_param(threshold, num_perm, false_positive_weight, false_negative_we
 				opt = (b, r)
 	return opt
 
-def read_base64(fname):
-	with open(fname, 'rb') as f:
-		content = f.read()
-	return content
-
-def write_base64(fname, content):
-	with open(fname, 'wb') as f:
-		f.write(content)
-
 class BloomTable:
 	"""
-	Interface to a Bloom Filter meant to model a single band of the signature matrix
+	Interface to a Bloom Filter meant to model a single band of the MinHash signature matrix
+
+	Args:
+		item_count (int): Number of items expected to be inserted (size of dataset). Used to create Bloom filter.
+		fp (float): False positive rate for Bloom filter in (0,1).
+		band_size (int): Size of band from MinHash signature matrix this filter is meant to model.
+		fname (str): File path where Bloom filter will be saved. If this file already exists, will initialize the Bloom filter from this path.
+		max_size (int): Maximum number of elements we should plan to insert into this Bloom filter. Upper bounds the size of the Bloom filter.
 	"""
-	def __init__(self, item_count: int, fp: float, num_arrays: int, fname: str = None, max_size: int = None, use_mmap: bool = True):
-		self.r = num_arrays
+	def __init__(self, item_count: int, fp: float, band_size: int, fname: str = None):
+		self.r = band_size
 		self.fname = fname
-		self.use_mmap = use_mmap
-		if max_size is not None and item_count > max_size:
-			item_count = max_size
 		if fname is not None and os.path.exists(fname):
 			print(f"Loading Bloom Filter at {fname}...")
-			if self.use_mmap:
-				self.bloom_filter = BloomFilter.open(fname)
-			else:
-				b64_repr = read_base64(fname)
-				self.bloom_filter = BloomFilter.from_base64("/tmp/temp.bf", b64_repr)
+			self.bloom_filter = BloomFilter.open(fname)
 		else:
 			self.bloom_filter = BloomFilter(
 				capacity=item_count, 
 				error_rate=fp, 
-				filename=self.fname if self.use_mmap else None
+				filename=self.fname
 			)
 
 	def sync(self):
-		if self.use_mmap:
+		if self.fname is not None:
 			self.bloom_filter.sync()
 		else:
-			b64_repr = self.bloom_filter.to_base64()
-			write_base64(self.fname, b64_repr)
+			warnings.warn("Attempting to save in-memory Bloom filter, this is a no-op.", RuntimeWarning)
 
 	def assert_size(self, hashvalues: List[int]):
 		if not len(hashvalues) == self.r:
-			raise RuntimeError(f"Invalid length for indices, {len(hashvalues)}, expected {self.r} items")
+			raise RuntimeError(f"Invalid length for indices, {len(hashvalues)}, expected {self.r} hashvalues in band")
 		
 	def insert(self, hashvalues: List[int]) -> None:
 		"""
 		Takes as input the indices for a single band and inserts them into the corresponding bit arrays
+
+		Args:
+			hashvalues (List[int]): The hashvalues from a single band of a MinHash object.
 		"""
 		self.assert_size(hashvalues)
 		# https://en.wikipedia.org/wiki/Universal_hashing#Hashing_vectors
 		# as the hashvalues are the result of a universal hashing function, their sum is also a univeral hash function
 		x = sum(hashvalues) % _mersenne_prime
-		self.bloom_filter.add(x)#.tobytes())
+		self.bloom_filter.add(x)
 
 	def query(self, hashvalues: List[int]) -> bool:
 		"""
 		Takes as input the indices for a single band and queries them against the corresponding arrays
 		returns True if the each query returns True, otherwise returns False
+
+		Args:
+			hashvalues (List[int]): The hashvalues from a single band of a MinHash object.
 		"""
 		self.assert_size(hashvalues)
 		x = sum(hashvalues) % _mersenne_prime
-		return x in self.bloom_filter #self.bloom_filter.check(x.tobytes())
+		return x in self.bloom_filter
 
 
 class MinHashLSHBloom(object):
 	"""
-	The :ref:`minhash_lsh` index.
+	The :ref:`lsh_bloom` index.
 	It supports query with `Jaccard similarity`_ threshold.
-	Reference: `Chapter 3, Mining of Massive Datasets
-	<http://www.mmds.org/>`_.
+	Reference: `LSHBloom
+	<https://arxiv.org/abs/2411.04257>`_.
 
 	Args:
 		threshold (float): The Jaccard similarity threshold between 0.0 and
-			1.0. The initialized MinHash LSH will be optimized for the threshold by
+			1.0. The initialized MinHash LSH index will be optimized for the threshold by
 			minizing the false positive and false negative.
 		num_perm (int): The number of permutation functions used
 			by the MinHash to be indexed. For weighted MinHash, this
 			is the sample size (`sample_size`).
+		n (int): The number of elements to be inserted (estimate of dataset size).
+		fp (float): The false positive rate for each Bloom filter. Must be in (0,1).
+		save_dir (str): The directory to save the Bloom filter index to. If Bloom filters
+			already exist in this directory, the index will be loaded from here. If None, 
+			an in-memory index will be created - this index can not be persisted.
 		weights (Tuple[float, float]): Used to adjust the relative importance of
 			minimizing false positive and false negative when optimizing
 			for the Jaccard similarity threshold.
@@ -126,24 +128,17 @@ class MinHashLSHBloom(object):
 			of each bands). This is used to bypass the parameter optimization
 			step in the constructor. `threshold` and `weights` will be ignored
 			if this is given.
-		storage_config (Optional[Dict]): Type of storage service to use for storing
-			hashtables and keys.
-			`basename` is an optional property whose value will be used as the prefix to
-			stored keys. If this is not set, a random string will be generated instead. If you
-			set this, you will be responsible for ensuring there are no key collisions.
-		prepickle (Optional[bool]): If True, all keys are pickled to bytes before
-			insertion. If not specified, a default value is chosen based on the
-			`storage_config`.
-		hashfunc (Optional[Callable[[bytes], bytes]]): If a hash function is provided it will be used to
-			compress the index keys to reduce the memory footprint. This could cause a higher
-			false positive rate.
 
 	Note:
-		`weights` must sum to 1.0, and the format is
-		(false positive weight, false negative weight).
-		For example, if minimizing false negative (or maintaining high recall) is more
-		important, assign more weight toward false negative: weights=(0.4, 0.6).
-		Try to live with a small difference between weights (i.e. < 0.5).
+		This algorithm is a space optimized version of MinHashLSH.
+		For more details on :ref:`minhash_lsh`, see the documentation. 
+		
+		This algorithm uses Bloom filters to drastically reduce the space
+		that the LSH index occupies on disk. However, it loses the ability
+		to retrieve candidate duplicate keys. Rather, it can only tell you
+		whether a query set is a duplicate of a set that was inserted previously.
+		This enables scaling to datasets of many hundreds of millions or billions
+		of documents, but may not be appropriate for all use cases.
 
 	Examples:
 
@@ -152,70 +147,54 @@ class MinHashLSHBloom(object):
 
 		.. code-block:: python
 
-			lsh = MinHashLSH(threshold=0.9, num_perm=128)
-			print(lsh.b, lsh.r)
-			# 5 25
+			from datasketch import MinHash, MinHashLSH
 
-		The built-in optimizer will try to minimize the weighted sum of
-		probabilities of false positive and false negative. The algorithm is
-		a simple grid search over the space of possible parameters.
+			set1 = set(['minhash', 'is', 'a', 'probabilistic', 'data', 'structure', 'for',
+            			'estimating', 'the', 'similarity', 'between', 'datasets'])
+			set2 = set(['minhash', 'is', 'a', 'probability', 'data', 'structure', 'for',
+						'estimating', 'the', 'similarity', 'between', 'documents'])
+			set3 = set(['minhash', 'is', 'probability', 'data', 'structure', 'for',
+						'estimating', 'the', 'similarity', 'between', 'documents'])
 
-		Note that it is possible to get :attr:`b` (number of bands) and
-		:attr:`r` (band size) that do not sum to :attr:`num_perm`, leading to
-		unused permutation values in the indexed MinHash.
-		This is because the optimizer only considers bands of
-		the same size, and the number of bands is not necessarily a divisor of
-		:attr:`num_perm`.
+			m1 = MinHash(num_perm=128)
+			m2 = MinHash(num_perm=128)
+			m3 = MinHash(num_perm=128)
+			for d in set1:
+				m1.update(d.encode('utf8'))
+			for d in set2:
+				m2.update(d.encode('utf8'))
+			for d in set3:
+				m3.update(d.encode('utf8'))
 
-		Instead of using the built-in optimizer, you can customize the LSH
-		parameters your self. The snippet below creates an index with 128
-		permutation functions and 16 bands each with size 8, skipping the
-		optimization step:
+			# Create LSHBloom index
+			lsh = MinHashLSHBloom(threshold=0.5, num_perm=128, n=100, fp=0.0001, save_dir="./index/")
+			lsh.insert(m2)
+			lsh.insert(m3)
 
-		.. code-block:: python
-
-			lsh = MinHashLSH(num_perm=128, params=(16, 8))
-			print(lsh.b, lsh.r)
-			# 16 8
-
-		Create an index backed by Redis storage:
-
-		.. code-block:: python
-
-			lsh = MinHashLSH(threshold=0.9, num_perm=128, storage_config={
-				'type': 'redis',
-				'basename': b'mylsh', # optional, defaults to a random string.
-				'redis': {'host': 'localhost', 'port': 6379},
-			})
-
-		The `basename` property is optional. It is used to generate key prefixes
-		in the storage layer to uniquely identify data associated with this LSH.
-		Thus, if you create a new LSH object with the same `basename`, you will
-		be using the same underlying data in the storage layer associated with
-		a previous LSH object. If you do not set this property, a random string
-		will be generated instead.
-
+			# Query whether m1 is a duplicate according to the given threshold
+			is_duplicate = lsh.query(m1)
 	"""
 
 	def __init__(
 		self,
 		threshold: float = 0.9,
 		num_perm: int = 128,
-		weights: Tuple[float, float] = (0.5, 0.5),
-		num_bits: int = 32, # size in bits of each hashvalue, minhashes will be truncated to this size via bBitMinHashing
 		n: int = None,
 		fp: float = None,
-		save_dir: str = None, # place to save bloom filter index, if it is filled we'll load the bloom filters from there
-		use_mmap: bool = True,
+		save_dir: str = None,
+		weights: Tuple[float, float] = (0.5, 0.5),
 		params: Optional[Tuple[int, int]] = None,
-		hashfunc: Optional[Callable[[bytes], bytes]] = None,
 	) -> None:
-		self._buffer_size = 50000
-		self.num_bits = num_bits
 		if threshold > 1.0 or threshold < 0.0:
 			raise ValueError("threshold must be in [0.0, 1.0]")
 		if num_perm < 2:
 			raise ValueError("Too few permutation functions")
+		if n <= 0:
+			raise ValueError("n for LSHBloom must be >= 0")
+		if fp >= 1.0 or fp <= 0.0:
+			raise ValueError("fp must be in (0.0, 1.0)")
+		if save_dir is None:
+			warnings.warn("Creating LSHBloom index without save directory, this index will not be persisted.", RuntimeWarning)
 		if any(w < 0.0 or w > 1.0 for w in weights):
 			raise ValueError("Weight must be in [0.0, 1.0]")
 		if sum(weights) != 1.0:
@@ -239,24 +218,14 @@ class MinHashLSHBloom(object):
 		if self.b < 2:
 			raise ValueError("The number of bands are too small (b < 2)")
 
-		self.hashfunc = hashfunc
-		if hashfunc:
-			self._H = self._hashed_byteswap
-		else:
-			self._H = self._byteswap
-
-		# create a bitarray for each signature row
+		# create a Bloom filter for each band in the signature matrix
 		if save_dir is not None:
 			os.makedirs(save_dir, exist_ok=True)
-		hashrange = 2**self.num_bits
-		max_size = self.r * hashrange
 		self.hashtables = [
 			BloomTable(
 					item_count=n, 
-					fp=fp, num_arrays=self.r, 
-					fname=os.path.join(save_dir, f"band-{i}.bf") if save_dir is not None else None, 
-					max_size=max_size,
-					use_mmap=use_mmap
+					fp=fp, band_size=self.r, 
+					fname=os.path.join(save_dir, f"band-{i}.bf") if save_dir is not None else None,
 				)
 			for i in range(self.b)
 		]
@@ -264,20 +233,14 @@ class MinHashLSHBloom(object):
 
 	def insert(
 		self,
-		minhash: MinHash,
-		check_duplication: bool = True,
+		minhash: MinHash
 	):
 		"""
 		Insert a key to the index, together with a MinHash or Weighted MinHash
 		of the set referenced by the key.
 
 		Args:
-			key (Hashable): The unique identifier of the set.
 			minhash (Union[MinHash, WeightedMinHash]): The MinHash of the set.
-			check_duplication (bool): To avoid duplicate keys in the storage
-				(`default=True`). It's recommended to not change the default, but
-				if you want to avoid the overhead during insert you can set
-				`check_duplication = False`.
 
 		"""
 		self._insert(minhash)
@@ -291,39 +254,32 @@ class MinHashLSHBloom(object):
 				"Expecting minhash with length %d, got %d" % (self.h, len(minhash))
 			)
 		
-		# truncate minhash to desired bit width
-		trunc_minhash = minhash
-		if self.num_bits <= 32:
-			trunc_minhash = bBitMinHash(minhash=minhash, b=self.num_bits)
-		Hs = [trunc_minhash.hashvalues[start:end] for start, end in self.hashranges]
+		Hs = [minhash.hashvalues[start:end] for start, end in self.hashranges]
 
 		for H, hashtable in zip(Hs, self.hashtables):
 			hashtable.insert(H)
 
 	def query(self, minhash) -> bool:
 		"""
-		Giving the MinHash of the query set, retrieve
-		the keys that reference sets with Jaccard
-		similarities likely greater than the threshold.
+		Giving the MinHash of the query set, determine
+		whether any previously inserted sets have 
+		Jaccard similarity with the query that is
+		likely greater than the threshold.
 
 		Results are based on minhash segment collision
-		and are thus approximate. For more accurate results,
-		filter again with :meth:`MinHash.jaccard`. For exact results,
-		filter by computing Jaccard similarity using original sets.
+		and are thus approximate.
 
 		Args:
 			minhash (MinHash): The MinHash of the query set.
 
 		Returns:
-			list: a list of unique keys.
+			bool: Whether the item is a duplicate or not, based on the given threshold.
 
 		Example:
 
-			Query and rank results using :meth:`MinHash.jaccard`.
-
 			.. code-block:: python
 
-				from datasketch import MinHash, MinHashLSH
+				from datasketch import MinHash, MinHashLSHBloom
 				import numpy as np
 
 				# Generate 100 random MinHashes.
@@ -332,28 +288,24 @@ class MinHashLSHBloom(object):
 					num_perm=128
 				)
 
-				# Create LSH index.
-				lsh = MinHashLSH(threshold=0.5, num_perm=128)
+				# Create LSHBloom index.
+				lsh = MinHashLSHBloom(threshold=0.5, num_perm=128, n=100, fp=0.0001, save_dir="./index/")
 				for i, m in enumerate(minhashes):
 					lsh.insert(i, m)
 
-				# Get the initial results from LSH.
+				# Get the duplication result from LSHBloom.
 				query = minhashes[0]
-				results = lsh.query(query)
-
-				# Rank results using Jaccard similarity estimated by MinHash.
-				results = [(query.jaccard(minhashes[key]), key) for key in results]
-				results.sort(reverse=True)
-				print(results)
+				is_duplicate = lsh.query(query)
+				print(is_duplicate)
 
 			Output:
 
 			.. code-block::
 
-				[(1.0, 0), (0.421875, 4), (0.4140625, 19), (0.359375, 58), (0.3359375, 78), (0.265625, 62), (0.2578125, 11), (0.25, 98), (0.171875, 21)]
+				True
 
 			Note that although the threshold is set to 0.5, the results are not
-			guaranteed to be above 0.5 because the LSH index is approximate and
+			guaranteed to be above 0.5 because the LSHBloom index is approximate and
 			the Jaccard similarity is estimated by MinHash.
 
 		"""
@@ -361,12 +313,10 @@ class MinHashLSHBloom(object):
 			raise ValueError(
 				"Expecting minhash with length %d, got %d" % (self.h, len(minhash))
 			)
+		
 		# if we match in any band, this is a candidate pair
-		trunc_minhash = minhash
-		if self.num_bits <= 32:
-			trunc_minhash = bBitMinHash(minhash=minhash, b=self.num_bits)
 		for (start, end), hashtable in zip(self.hashranges, self.hashtables):
-			H = trunc_minhash.hashvalues[start:end]
+			H = minhash.hashvalues[start:end]
 			collision = hashtable.query(H)
 			if collision:
 				return True
@@ -376,10 +326,3 @@ class MinHashLSHBloom(object):
 		print("Saving Bloom Index...")
 		for table in self.hashtables:
 			table.sync()
-
-
-	def _hashed_byteswap(self, hs):
-		return self.hashfunc(bytes(hs.byteswap().data))
-
-	def _byteswap(self, hs):
-		return bytes(hs.byteswap().data)
--- test/test_lshbloom.py
+++ test/test_lshbloom.py
@@ -1,5 +1,6 @@
 import unittest
 import pickle
+from glob import glob
 from datasketch.lsh_bloom import BloomTable, MinHashLSHBloom
 from datasketch.minhash import MinHash
 import numpy as np
@@ -8,56 +9,30 @@ import os
 class TestBloomTable(unittest.TestCase):
 	def test_insert(self):
 		r = 3
-		sz = 32
 		x = np.array([2,3,31], dtype=np.uint32)
-		b = BloomTable(10, 0.01, num_arrays=r)
+		b = BloomTable(10, 0.01, band_size=r)
 		b.insert(x)
 		self.assertRaises(RuntimeError, b.insert, np.array([2,2], dtype=np.uint32))
 
 	def test_query(self):
 		r = 3
-		sz = 32
 		x = np.array([2,3,31], dtype=np.uint32)
-		b = BloomTable(10, 0.01, num_arrays=r)
+		b = BloomTable(10, 0.01, band_size=r)
 		b.insert(x)
 		self.assertTrue(b.query(x))
 		self.assertFalse(b.query(np.array([2,3,30], dtype=np.uint32)))
 		self.assertRaises(RuntimeError, b.query, [2,2])
 
-	def test_sync_in_memory(self):
-		fname = "/tmp/bloomfilter-mem.bf"
-		if os.path.exists(fname):
-			os.remove(fname)
-		r = 3
-		sz = 32
-		x = np.array([2,3,31], dtype=np.uint32)
-		y = np.array([12,10,29], dtype=np.uint32)
-		z = np.array([27,30,8], dtype=np.uint32)
-		items = [x,y,z]
-		b = BloomTable(10, 0.01, num_arrays=r, fname=fname, use_mmap=False)
-		for item in items:
-			b.insert(item)
-		for item in items:
-			self.assertTrue(b.query(item))
-		b.sync()
-
-		del b
-
-		b_ = BloomTable(10, 0.01, num_arrays=r, fname=fname, use_mmap=False)
-		for item in items:
-			self.assertTrue(b_.query(item))
-
-	def test_sync_mmap(self):
+	def test_save(self):
 		fname = "/tmp/bloomfilter.bf"
 		if os.path.exists(fname):
 			os.remove(fname)
 		r = 3
-		sz = 32
 		x = np.array([2,3,31], dtype=np.uint32)
 		y = np.array([12,10,29], dtype=np.uint32)
 		z = np.array([27,30,8], dtype=np.uint32)
 		items = [x,y,z]
-		b = BloomTable(10, 0.01, num_arrays=r, fname=fname, use_mmap=True)
+		b = BloomTable(10, 0.01, band_size=r, fname=fname)
 		for item in items:
 			b.insert(item)
 		for item in items:
@@ -66,7 +41,7 @@ class TestBloomTable(unittest.TestCase):
 
 		del b
 
-		b_ = BloomTable(10, 0.01, num_arrays=r, fname=fname, use_mmap=True)
+		b_ = BloomTable(10, 0.01, band_size=r, fname=fname)
 		for item in items:
 			self.assertTrue(b_.query(item))
 
@@ -74,9 +49,9 @@ class TestBloomTable(unittest.TestCase):
 class TestMinHashLSHBloom(unittest.TestCase):
 
 	def test_init(self):
-		lsh = MinHashLSHBloom(threshold=0.8, num_bits=16, n=10, fp=0.01)
+		lsh = MinHashLSHBloom(threshold=0.8, n=10, fp=0.01)
 		b1, r1 = lsh.b, lsh.r
-		lsh = MinHashLSHBloom(threshold=0.8, weights=(0.2,0.8), num_bits=16, n=10, fp=0.01)
+		lsh = MinHashLSHBloom(threshold=0.8, weights=(0.2,0.8), n=10, fp=0.01)
 		b2, r2 = lsh.b, lsh.r
 		self.assertTrue(b1 < b2)
 		self.assertTrue(r1 > r2)
@@ -84,7 +59,7 @@ class TestMinHashLSHBloom(unittest.TestCase):
 		
 
 	def test_insert(self):
-		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, num_bits=16, n=10, fp=0.01)
+		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, n=10, fp=0.01)
 		m1 = MinHash(16)
 		m1.update("a".encode("utf8"))
 		m2 = MinHash(16)
@@ -96,7 +71,7 @@ class TestMinHashLSHBloom(unittest.TestCase):
 		self.assertRaises(ValueError, lsh.insert, m3)
 
 	def test_query(self):
-		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, num_bits=16, n=10, fp=0.01)
+		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, n=10, fp=0.01)
 		m1 = MinHash(16)
 		m1.update("a".encode("utf8"))
 		m2 = MinHash(16)
@@ -111,33 +86,40 @@ class TestMinHashLSHBloom(unittest.TestCase):
 		m3 = MinHash(18)
 		self.assertRaises(ValueError, lsh.query, m3)
 
-	def test_8bit(self):
-		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, num_bits=8, n=10, fp=0.01)
+	def test_save(self):
+		save_path = "./test_save/"
+		for item in glob(f"{save_path}/*.bf"):
+			os.remove(item)
+
+		lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, n=10, fp=0.01, save_dir=save_path)
 		m1 = MinHash(16)
 		m1.update("a".encode("utf8"))
 		m2 = MinHash(16)
 		m2.update("b".encode("utf8"))
 		lsh.insert(m1)
 		lsh.insert(m2)
-		result = lsh.query(m1)
+		lsh.sync()
+
+		lsh2 = MinHashLSHBloom(threshold=0.5, num_perm=16, n=10, fp=0.01, save_dir=save_path)
+		result = lsh2.query(m1)
 		self.assertTrue(result)
-		result = lsh.query(m2)
+		result = lsh2.query(m2)
 		self.assertTrue(result)
 
-	# def test_pickle(self):
-	# 	lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, num_bits=16, n=10, fp=0.01)
-	# 	m1 = MinHash(16)
-	# 	m1.update("a".encode("utf8"))
-	# 	m2 = MinHash(16)
-	# 	m2.update("b".encode("utf8"))
-	# 	lsh.insert(m1)
-	# 	lsh.insert(m2)
-	# 	lsh2 = pickle.loads(pickle.dumps(lsh))
+	def test_save_in_memory(self):
 
-	# 	result = lsh2.query(m1)
-	# 	self.assertTrue(result)
-	# 	result = lsh2.query(m2)
-	# 	self.assertTrue(result)
+		with self.assertWarns(RuntimeWarning):
+			lsh = MinHashLSHBloom(threshold=0.5, num_perm=16, n=10, fp=0.01, save_dir=None)
+
+		m1 = MinHash(16)
+		m1.update("a".encode("utf8"))
+		m2 = MinHash(16)
+		m2.update("b".encode("utf8"))
+		lsh.insert(m1)
+		lsh.insert(m2)
+
+		with self.assertWarns(RuntimeWarning):
+			lsh.sync()
 
 
 if __name__ == "__main__":
