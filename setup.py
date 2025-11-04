2fb5997d25cb31e7a1a55ff857635296781f3166
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2025-11-04 06:54:13 +0000

    Drop Python 3.7 support, add Python 3.12 support
    - Remove Python 3.7 classifier from setup.py
    - Add Python 3.12 classifier to setup.py
    - Update CI matrix to include Python 3.12, remove 3.7
    - Update Travis CI config to match supported versions
    - Update README.rst minimum version requirement to 3.8+

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     keywords='database datamining',
     packages=find_packages(include=['datasketch*']),
