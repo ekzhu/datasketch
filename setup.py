b87c237944415646795ab57b7e97e5d3bd14b01d
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2025-11-05 01:29:09 +0000

    Revert "Update Python version matrix in CI workflow and add support for Python 3.13 and 3.14 in setup.py"
    This reverts commit d15fbf395f45fe91258f1fc7cce8f5c331e36b46.

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
     ],
     keywords='database datamining',
     packages=find_packages(include=['datasketch*']),
